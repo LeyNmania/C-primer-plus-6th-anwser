f027c6fdcbc95b67342c25d37665b1785cfe5469
LeyN <z1010078345@163.com>
2018-11-12 23:00:34 +0800

    Create 5.6.c

---
+++ chp5/5.6.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+ 
+int main()
+{
+    int day, sum = 0;
+    int i;
+ 
+    printf("Please input days: ");
+    scanf("%d", &day);
+ 
+    for (i = 1; i <= day; i++)
+    {
+        sum += i * i;
+    }
+ 
+    printf ("sum = %d", sum);
+ 
+    return 0;
+}
